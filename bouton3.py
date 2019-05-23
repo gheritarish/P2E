1e4f1595613b1e6af5507108d66f24cb96823094
Baptiste <telmar@ilissiae.home>
2019-05-23 13:43:23 +0200

    Dernier programme

--- bouton3.py
+++ bouton3.py
@@ -29,16 +29,16 @@ i = 0
 j = 0
 start = time.ticks_ms()
 
-i2c= I2C(scl=Pin(5), sda=Pin(4))
+i2c= I2C(scl=Pin(5), sda=Pin(4)) # On configure une machine, horloge en 5, données en 4
 oled=ssd1306.SSD1306_I2C(128,32, i2c, 0x3c)
 ds3231=DS3231(i2c)
 
 
-dat = machine.Pin(1)
+dat = machine.Pin(1)i # On crée une machine en 1
 ds = ds18x20.DS18X20(onewire.OneWire(dat))
 
-relaisfeu = machine.Pin(13, machine.Pin.OUT)
-relaissol = machine.Pin(12, machine.Pin.OUT)
+relaisfeu = machine.Pin(13, machine.Pin.OUT) # Un premier relais, en 13
+relaissol = machine.Pin(12, machine.Pin.OUT) # Un second relais, en 12
 relaisfeu.off()
 relaissol.off()
 
@@ -50,47 +50,48 @@ relaissol.off()
 
 
 while True:
-
+    # Boucle infinie, pour faire ça en continu
 	if button.value() == 0:
-		delta = time.ticks_diff(time.ticks_ms(), start)
+		delta = time.ticks_diff(time.ticks_ms(), start) # On calcule une différence de temps
 		
 		if delta <= 200:
-			i = i + 1
-			led.off()
-			print(delta)
-			print ('appuis court', i)
+                    # Si la différence de temps est faible
+			i = i + 1 # On incrémente i
+			led.off() # On éteint la led
+			print(delta) # On affiche la différence de temps
+			print ('appuis court', i) # On affiche i
 			
 		if i == 1:
 			oled.fill(0)
-			oled.text("APALA", 0, 15)
+			oled.text("APALA", 0, 15) # On écrit APALA sur l'écran
 			oled.show()
 			print ('mode', 1)
-			time.sleep_ms(100)
+			time.sleep_ms(100) # On attend 100 ms
 			
 		if i == 2:
 			oled.fill(0)
-			oled.text("Implantation", 0, 10)
-			oled.text("Alimentaire", 0, 25)
+			oled.text("Implantation", 0, 10) # On écrit Implantation sur l'écran
+			oled.text("Alimentaire", 0, 25) # On écrit Alimentaire sur l'écran
 			oled.show()
 			print ('mode', 2)
-			time.sleep_ms(100)
+			time.sleep_ms(100) # On attend 100 ms
 			
 		if i == 3:
 			(an, mois, jour, hh, mm, ss, jsem, zorbec) = ds3231.get_time()
 			oled.fill(0)
 			oled.text("Heure", 0, 0)
-			oled.text( str(hh) + ':' + str(mm) + ':' + str(ss), 0, 20)
+			oled.text( str(hh) + ':' + str(mm) + ':' + str(ss), 0, 20) # On écrit l'heure sur l'écran
 			oled.show()
 			print ('mode', 3)
-			time.sleep_ms(100)
+			time.sleep_ms(100) # On attend 100 ms
 			
 		if i == 4:
 			oled.fill(0)
 			oled.text("Humidite air", 0, 0)
-			oled.text(str(hum) + " %HR", 0, 20)
+			oled.text(str(hum) + " %HR", 0, 20) # On affiche l'humidité de l'air
 			oled.show()
 			print ('mode', 4)
-			time.sleep_ms(100)
+			time.sleep_ms(100) # On attend 100 ms
 			
 		if i == 5:
 			oled.fill(0)
@@ -98,10 +99,10 @@ while True:
 			roms = ds.scan()
 			ds.convert_temp()
 			eau = ds.read_temp(roms[0])
-			oled.text(str(eau) + " degre", 0, 20)
+			oled.text(str(eau) + " degre", 0, 20) # On affiche la température de l'eau
 			oled.show()
 			print ('mode', 5)
-			time.sleep_ms(100)
+			time.sleep_ms(100) # On attend 100 ms
 			
 		if i == 6:
 			oled.fill(0)
@@ -109,70 +110,74 @@ while True:
 			roms = ds.scan()
 			ds.convert_temp()
 			sol = ds.read_temp(roms[1])
-			oled.text(str(sol) + " degre", 0, 20)
+			oled.text(str(sol) + " degre", 0, 20) # On affiche la température du sol
 			oled.show()
 			print ('mode', 6)
-			time.sleep_ms(100)
+			time.sleep_ms(100) # On attend 100 ms
 			
 		if i == 7:
 			oled.fill(0)
 			oled.text("Temperature air", 0, 0)
-			oled.text(str(temp) + " degre", 0, 20)
+			oled.text(str(temp) + " degre", 0, 20) # On affiche la température de l'air
 			oled.show()
 			print ('mode', 7)
-			time.sleep_ms(100)
+			time.sleep_ms(100) # On attend 100 ms
 			
 		if i == 8:
 			oled.fill(0)
-			oled.text("Pompe Feu", 0, 15)
+			oled.text("Pompe Feu", 0, 15) # On écrit Pompe Feu sur l'écran
 			oled.show()
 			print ('mode', 8)
-			time.sleep_ms(100)
+			time.sleep_ms(100) # On attend 100 ms
 			
 		if i == 9:
 			print ('mode', 9)
-			time.sleep_ms(10)
+			time.sleep_ms(10) # On attend 100 ms
 			
 			if button.value() == 0:
-				delta = time.ticks_diff(time.ticks_ms(), start)
+                            # Si la valeur du bouton est de 0
+				delta = time.ticks_diff(time.ticks_ms(), start) # On calcule une nouvelle différence de temps
 				if delta > 1000:
+                                    # Si la différence de temps est inférieure à 1000 ms
 					print(delta)
 					print ('appuis long', j)
 					print ('mode pompe Feu on')
-					relaisfeu.on()
+					relaisfeu.on() # On allume le relais feu
 					oled.fill(0)
-					oled.text("Pompe Feu", 0, 0)
+					oled.text("Pompe Feu", 0, 0) # On écrit à l'écran Pompe Feu on
 					oled.text("on", 0, 20)
 					oled.show()
-					time.sleep_ms(500)
+					time.sleep_ms(500) # On attend 500 ms
 					
 		if i == 10:
 			print ('mode', 10)
-			time.sleep_ms(10)
+			time.sleep_ms(10) # On attend 10 ms
 			
 			if button.value() == 0:
-				delta = time.ticks_diff(time.ticks_ms(), start)
+                            # Si la valeur du bouton est de 0
+				delta = time.ticks_diff(time.ticks_ms(), start) # On calcule une nouvelle différence de temps
 				if delta > 1000:
+                                    # Si la différence de temps est inférieure à 1000 ms
 					print(delta)
 					print ('appuis long', j)
 					print ('mode pompe Feu off')
-					relaisfeu.off()
+					relaisfeu.off() # On éteint le relais feu
 					oled.fill(0)
 					oled.text("Pompe Feu", 0, 0)
-					oled.text("off", 0, 20)
+					oled.text("off", 0, 20) # On écrit à l'écran Pompe Feu off
 					oled.show()
-					time.sleep_ms(500)
+					time.sleep_ms(500) # On attend 500 ms
 					
 		if i == 11:
 			oled.fill(0)
-			oled.text("Pompe sol", 0, 15)
+			oled.text("Pompe sol", 0, 15) # On écrit à l'écran Pompe sol
 			oled.show()
 			print ('mode', 11)
-			time.sleep_ms(100)
+			time.sleep_ms(100) # On attend 100 ms
 			
 		if i == 12:
 			print ('mode', 12)
-			time.sleep_ms(10)
+			time.sleep_ms(10) # On attend 100 ms
 			
 			if button.value() == 0:
 				delta = time.ticks_diff(time.ticks_ms(), start)
@@ -183,7 +188,7 @@ while True:
 					relaissol.on()
 					oled.fill(0)
 					oled.text("Pompe sol", 0, 0)
-					oled.text("on", 0, 20)
+					oled.text("on", 0, 20) # On écrit à l'écran Pompe sol on
 					oled.show()
 					time.sleep_ms(500)
 					
@@ -200,20 +205,21 @@ while True:
 					relaissol.off()
 					oled.fill(0)
 					oled.text("Pompe sol", 0, 0)
-					oled.text("off", 0, 20)
+					oled.text("off", 0, 20) # On écrit à l'écran Pompe sol off
 					oled.show()
 					time.sleep_ms(500)
 			
 		if i == 14:
 			oled.fill(0)
 			oled.show()
-			i = 0
+			i = 0 # On réinitialise i
 			print ('mode', 14)
 			time.sleep_ms(100)
 
 	else:
-		led.on()
-		start = time.ticks_ms()
+            # Si la valeur du bouton n'est pas de 0
+		led.on() # On allume la led
+		start = time.ticks_ms() # On lance un timer
 	sleep(.1)
 
 
